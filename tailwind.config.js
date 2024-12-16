b8bf6c9c9624b7acefed8cbbee273afaf807cd02
radeniss <mohammad.220170208@mhs.unimal.ac.id>
2024-12-16 09:37:17 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
+    theme: {
+        container: {
+            center: true,
+            padding: '16px',
+        },
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+
+            colors: {
+                primary: '#0284c7',
+                dark: '#0f172a'
+            },
+            screens: {
+                '2xl': '1230px',
+            },
+        },
+    },
+    plugins: [],
+};
